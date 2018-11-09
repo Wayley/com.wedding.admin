76221c46a1fdb1c7548d79f2f17f7bd54a185aaa
wzheng <hb_wangzheng@163.com>
2018-11-09 18:24:11 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>欢迎来到wedding.com 系统后台</p>
+    <Welcome :name="name"></Welcome>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Welcome from './components/Welcome.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Welcome
+  },
+  data(){
+    return{
+      name:'wz'
+    }
   }
 }
 </script>
